0b030b8e9b92b7b59ff255fa68119615d063568f
Sinh Fu San <ss12932@my.bristol.ac.uk>
2022-08-10 17:48:15 +0100

    memory leak fix example done

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import UseRefExample2 from './components/UseRefExample2';
+import UseRefExample3 from './components/UseRefExample3';
 function App() {
   return (
     <div className='container mt-5'>
-      <UseRefExample2 />
+      <UseRefExample3 />
     </div>
   );
 }
